d6067768ab781e2d1a7725dc37a68fb6033ba42f
ichoukri <ichoukri1337@gmail.com>
2023-04-12 17:55:14 +0000

    add auth module and set signup and signin endpoint

---
+++ nest-project/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{
+    signup() {
+        return "i am signed up";
+    }
+
+    signin() {
+        return "i am signed in";
+    }
+}
